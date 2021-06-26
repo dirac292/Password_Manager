5d381a45bcef23d47b1e31b811790d719a26bad9
Mahip Singh <72610388+dirac292@users.noreply.github.com>
2021-06-26 18:52:50 +0530

    New Features Added
    Master Password Login.
    Search Feature
    Password Stored in JSON file after decryption
    Automatic Deletion of Decrypted File after 1 min of search Passwords or closing the Program.
    Ttk themes for UI.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import re
 import base64
 import random
 import os
@@ -5,14 +7,26 @@ from cryptography.fernet import Fernet
 from tkinter import *
 from tkinter import messagebox
 from tkinter import simpledialog
+from tkinter import ttk
+from ttkthemes import ThemedTk
 import pyperclip
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
+import os.path
+import time
+from threading import Event
+import hash
+# import matplotlib
+# matplotlib.use('Agg')
 
 
 # ---------------------------- PASSWORD AND KEY GENERATOR ------------------------------- #
+
+# if os.environ.get('DISPLAY', '') == '':
+#     print('no display found. Using :0.0')
+#     os.environ.__setitem__('DISPLAY', ':0.0')
+
 
 def gen_key(master_pass):
 
@@ -31,6 +45,7 @@ def gen_key(master_pass):
     key = base64.urlsafe_b64encode(kdf.derive(password))
     return key
 
+
 def gen_pass():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
                'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
@@ -38,34 +53,63 @@ def gen_pass():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    pass_let = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    pass_sym = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    pass_num = [random.choice(numbers) for _ in range(random.randint(2, 4))]
 
-    pass_let=[random.choice(letters) for _ in range(random.randint(8, 10))]
-    pass_sym=[random.choice(symbols) for _ in range(random.randint(2, 4))]
-    pass_num=[random.choice(numbers) for _ in range(random.randint(2, 4))]
-
-    pass_list=pass_let+pass_sym+pass_num
+    pass_list = pass_let+pass_sym+pass_num
 
     random.shuffle(pass_list)
 
     password = "".join(pass_list)
 
-    password_entry.insert(0,password)
+    password_entry.insert(0, password)
     pyperclip.copy(password)
 
 
+def reset_pass():
+    if os.path.isfile("encrypted_data.txt"):
+        os.remove("encrypted_data.txt")
+        if os.path.isfile('data.json'):
+            os.remove("data.json")
+        messagebox.showinfo(
+            title="Prompt", message="All Password directories cleared.")
+    else:
+        messagebox.showinfo(
+            title="Prompt", message="Directories already empty.")
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+        # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def del_f():
+    if messagebox.askokcancel("Prompt", "Are you sure you want to delte the file?"):
+        if os.path.isfile('data.json'):
+            os.remove("data.json")
+            messagebox.showinfo(
+                title="Prompt", message="File Successfuly Removed.")
+        else:
+            messagebox.showinfo(
+                title="Prompt", message="File is not on the system.")
+
+
+def new_del():
+    if os.path.isfile('data.json'):
+        os.remove("data.json")
+
+
 def encryp(key):
 
     cipher = Fernet(key)
 
-    with open("data.txt", 'rb') as f:
+    with open("data.json", 'rb') as f:
         e_file = f.read()
     encrypted_file = cipher.encrypt(e_file)
     with open("encrypted_data.txt", 'wb') as ef:
         ef.write(encrypted_file)
+        print("written")
 
-    os.remove("data.txt")
+    os.remove("data.json")
+
 
 def decryp(key):
 
@@ -76,88 +120,362 @@ def decryp(key):
 
     decrypted_file = cipher.decrypt(encrypted_data)
 
-    with open('data.txt', 'wb') as df:
+    with open('data.json', 'wb') as df:
         df.write(decrypted_file)
 
-def view_txt():
-    master_pass = simpledialog.askstring(title='Test', prompt='Enter the master password?',show="*")
-    if master_pass == "password":
-        key = gen_key(master_pass)
-        decryp(key)
-        messagebox.showinfo(title="Prompt", message="Close the Program to view the File.Please Delete the file after use.")
-    else:
-        messagebox.showinfo(title="Oops", message="Check password again")
+
+# def view_txt():
+#     # some code to time
+
+#     if os.path.isfile("encrypted_data.txt"):
+#         master_pass = simpledialog.askstring(
+#             title='Test', prompt='Enter the master password?', show="*")
+#         if hash.check_pass(master_pass):
+#             key = gen_key(master_pass)
+
+#             decryp(key)
+#             messagebox.showinfo(
+#                 title="Prompt", message="File will be delted in a minute automatically.")
+#             os.system("start " + "data.json")
+
+#         else:
+#             messagebox.showinfo(title="Oops", message="Check password again")
+#     else:
+#         messagebox.showinfo(
+#             title="Prompt", message="Password Directory Empty.")
+
+#     window.after(60000, new_del)
+    # time.sleep(25)
+    # new_del()
+
+
+# ---------------------------- AUTO COMPLETE FEATURE ------------------------------- #
+# Class Autocomplete Code Credits: uroshekic  https: // gist.github.com/uroshekic/11078820 #
+
+
+class AutocompleteEntry(ttk.Entry):
+    def __init__(self, autocompleteList, *args, **kwargs):
+        # Listbox length
+        if 'listboxLength' in kwargs:
+            self.listboxLength = kwargs['listboxLength']
+            del kwargs['listboxLength']
+        else:
+            self.listboxLength = 8
+
+        # Custom matches function
+        if 'matchesFunction' in kwargs:
+            self.matchesFunction = kwargs['matchesFunction']
+            del kwargs['matchesFunction']
+        else:
+            def matches(fieldValue, acListEntry):
+                pattern = re.compile(
+                    '.*' + re.escape(fieldValue) + '.*', re.IGNORECASE)
+                return re.match(pattern, acListEntry)
+
+            self.matchesFunction = matches
+
+        ttk.Entry.__init__(self, *args, **kwargs)
+        self.focus()
+
+        self.autocompleteList = autocompleteList
+
+        self.var = self["textvariable"]
+        if self.var == '':
+            self.var = self["textvariable"] = StringVar()
+
+        self.var.trace('w', self.changed)
+        self.bind("<Right>", self.selection)
+        self.bind("<Up>", self.moveUp)
+        self.bind("<Down>", self.moveDown)
+
+        self.listboxUp = False
+
+    def changed(self, name, index, mode):
+        if self.var.get() == '':
+            if self.listboxUp:
+                self.listbox.destroy()
+                self.listboxUp = False
+        else:
+            words = self.comparison()
+            if words:
+                if not self.listboxUp:
+                    self.listbox = Listbox(
+                        width=self["width"], height=self.listboxLength)
+                    self.listbox.bind("<Button-1>", self.selection)
+                    self.listbox.bind("<Right>", self.selection)
+                    self.listbox.place(
+                        x=self.winfo_x(), y=self.winfo_y() + self.winfo_height())
+                    self.listboxUp = True
+
+                self.listbox.delete(0, END)
+                for w in words:
+                    self.listbox.insert(END, w)
+            else:
+                if self.listboxUp:
+                    self.listbox.destroy()
+                    self.listboxUp = False
+
+    def selection(self, event):
+        if self.listboxUp:
+            self.var.set(self.listbox.get(ACTIVE))
+            self.listbox.destroy()
+            self.listboxUp = False
+            self.icursor(END)
+
+    def moveUp(self, event):
+        if self.listboxUp:
+            if self.listbox.curselection() == ():
+                index = '0'
+            else:
+                index = self.listbox.curselection()[0]
+
+            if index != '0':
+                self.listbox.selection_clear(first=index)
+                index = str(int(index) - 1)
+
+                self.listbox.see(index)  # Scroll!
+                self.listbox.selection_set(first=index)
+                self.listbox.activate(index)
+
+    def moveDown(self, event):
+        if self.listboxUp:
+            if self.listbox.curselection() == ():
+                index = '0'
+            else:
+                index = self.listbox.curselection()[0]
+
+            if index != END:
+                self.listbox.selection_clear(first=index)
+                index = str(int(index) + 1)
+
+                self.listbox.see(index)  # Scroll!
+                self.listbox.selection_set(first=index)
+                self.listbox.activate(index)
+
+    def comparison(self):
+        return [w for w in self.autocompleteList if self.matchesFunction(self.var.get(), w)]
+
+
+autocompleteList = ['Gmail', 'YouTube', 'Facebook',
+                    'Zoom', 'Reddit', 'Netflix', 'Microsoft', 'Amazon', 'Instagram', 'Google', 'Twitch', 'Twitter', 'Apple Inc', 'Adobe', 'Linkedin',
+                    'Hotstar', 'Quora', 'Dropbox']
+
+
+def matches(fieldValue, acListEntry):
+    pattern = re.compile(re.escape(fieldValue) + '.*', re.IGNORECASE)
+    return re.match(pattern, acListEntry)
+
+
+def is_present(arr, entry):
+    for i in arr:
+        if i == entry:
+            return True
+    return False
+
 
 def save_pass():
 
-    website=website_entry.get()
-    email=email_entry.get()
-    password=password_entry.get()
+    website = website_entry.get()
+    if(not (is_present(autocompleteList, website))):
+        autocompleteList.append(website)
+
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "username": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        messagebox.showinfo(
+            title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok=messagebox.askokcancel(title="website",message=f"These are the details entered:\n Email: {email}"
-                               f"\nPassword:{password} \nIs it ok to save?")
+        is_ok = messagebox.askokcancel(title=f"{website}", message=f"These are the details entered:\n Email: {email}"
+                                       f"\n Password: {password} \n Is it ok to save?")
 
         if is_ok:
-            master_pass = simpledialog.askstring(title='Test', prompt='Enter the master password?',show="*")
-            #Sample Password For now
-            if master_pass=="password":
-                key=gen_key(master_pass)
-                decryp(key)
-                with open("data.txt","a") as data_file:
-                    data_file.write(f"{website} | {email} | {password}\n")
-                    website_entry.delete(0,END)
-                    password_entry.delete(0,END)
-                encryp(key)
+            master_pass = simpledialog.askstring(
+                title='Test', prompt='Enter the master password?', show="*")
+            # Sample Password For now
+            if hash.check_pass(master_pass):
+                key = gen_key(master_pass)
+                if os.path.isfile('encrypted_data.txt'):
+                    decryp(key)
+                if (not os.path.isfile('data.json')):
+                    with open("data.json", "w") as data_file:
+                        json.dump(new_data, data_file, indent=4)
+                        website_entry.delete(0, END)
+                        password_entry.delete(0, END)
+                    encryp(key)
+                else:
+                    with open("data.json", 'r') as data_file:
+                        data = json.load(data_file)
+                    data.update(new_data)
+
+                    with open("data.json", "w") as data_file:
+                        json.dump(data, data_file, indent=4)
+                        website_entry.delete(0, END)
+                        password_entry.delete(0, END)
+                    encryp(key)
             else:
-                messagebox.showinfo(title="Oops", message="Check password again")
+                messagebox.showinfo(
+                    title="Oops", message="Check password again")
 
 
+def on_closing():
+
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        window.destroy()
+        new_del()
 
 
+def create_login(create_pass):
+    # os.remove('encrypted_data.txt')
+    hash.create_pass(create_pass)
+    messagebox.showinfo(
+        title="Prompt", message="Password Sucessfuly Created")
 
 
+def searchpass():
+    website = website_entry.get()
+    print(website)
+    if(website == ""):
+        messagebox.showinfo(title="Prompt",
+                            message="Please Don't leave the website entry empty.")
+    else:
 
-# ---------------------------- UI SETUP ------------------------------- #
+        if os.path.isfile("encrypted_data.txt"):
+            master_pass = simpledialog.askstring(
+                title='Prompt', prompt='Enter the master password?', show="*")
+            if hash.check_pass(master_pass):
+                key = gen_key(master_pass)
 
-window = Tk()
+                decryp(key)
+                with open("data.json") as data_file:
+                    data = json.load(data_file)
+                    if website in data:
+                        email = data[website]["username"]
+                        password = data[website]["password"]
+                        pyperclip.copy(password)
+                        messagebox.showinfo(title=website,
+                                            message=f"Username: {email}\nPassword: {password}\nCopied to Clipboard")
+                    else:
+                        messagebox.showinfo(
+                            title="Error Occured", message=f"No Username or Password Found for the {website}.")
+
+            else:
+                messagebox.showinfo(
+                    title="Oops", message="Check password again")
+        else:
+            messagebox.showinfo(
+                title="Prompt", message="Password Directory Empty.")
+
+    window.after(60000, new_del)
+
+
+start = time.time()
+incorrect_tries = 0
+window = ThemedTk(theme="arc")
+style = ttk.Style(window)
+style.theme_use("xpnative")
+# window.get_themes()
+# window.set_theme("clearlooks")
 window.title("Password Manager")
-window.config(padx=50,pady=50)
+window.iconbitmap(r'padlock.ico')
+window.state("zoomed")
+window.geometry("1000x1000")
+back_image = PhotoImage(file="new.png")
 
-canvas=Canvas(width=200,height=200)
-pass_img=PhotoImage(file="download.png")
-canvas.create_image(100,100,image=pass_img)
-canvas.grid(row=0,column=1)
+if(not os.path.isfile("hashed_pass.txt")):
+    master_pass = simpledialog.askstring(
+        title='Register', prompt='Create Master Password', show="*")
+    create_login(master_pass)
 
-website_label=Label(text="Website:")
-website_label.grid(row=1,column=0)
-email_label=Label(text="Email/Username:")
-email_label.grid(row=2,column=0)
-pass_label=Label(text="Password:")
-pass_label.grid(row=3,column=0)
+master_pass = simpledialog.askstring(
+    title='Test', prompt='Enter the master password?', show="*", parent=window)
 
-
-#Entries
-
-website_entry= Entry(width=35)
-website_entry.grid(row=1,column=1,columnspan=2)
-website_entry.focus()
-email_entry= Entry(width=35)
-email_entry.grid(row=2,column=1,columnspan=2)
-email_entry.insert(0,"abc@gmail.com")
-password_entry= Entry(width=21)
-password_entry.grid(row=3,column=1)
-
-gen_pass=Button(text="Generate Password",command=gen_pass)
-gen_pass.grid(row=3,column=2)
-add_button=Button(text="Add",width=35,command=save_pass)
-add_button.grid(row=4,column=1,columnspan=2)
-
-view_pass=Button(text="View Passwords in a Text File",command=view_txt)
-view_pass.grid(row=5,column=1)
+if(master_pass == None):
+    messagebox.showinfo(
+        title="Prompt", message="Error Occured")
+    window.destroy()
+    exit()
+    # ---------------------------- UI SETUP ------------------------------- #
 
 
+while(incorrect_tries <= 2):
 
-window.mainloop()
+    if hash.check_pass(master_pass):
+
+        canvas = Canvas(window, width=1000, height=1000)
+        canvas.pack(fill="both", expand=True)
+
+        canvas.create_image(0, 0, image=back_image, anchor="nw")
+        canvas.create_text(500, 150, text="ManagePass",
+                           font=("Helvetica", 45), fill="white")
+
+        canvas.create_text(500, 250, text="Website: ",
+                           font=("Helvetica"), fill="white")
+        canvas.create_text(500, 280, text="Username:",
+                           font=("Helvetica"), fill="white")
+        canvas.create_text(500, 310, text="Password:",
+                           font=("Helvetica"), fill="white")
+
+        # # Entries
+        website_entry = AutocompleteEntry(
+            autocompleteList, window, listboxLength=6, width=35, matchesFunction=matches)
+
+        website_entry_window = canvas.create_window(
+            550, 240, anchor="nw", window=website_entry)
+
+        email_entry = ttk.Entry(window, width=35)
+        email_entry_window = canvas.create_window(
+            550, 270, anchor="nw", window=email_entry)
+
+        password_entry = ttk.Entry(window, width=21, show="*")
+        password_entry_window = canvas.create_window(
+            550, 300, anchor="nw", window=password_entry)
+
+        search_pass = ttk.Button(window, text="Search", command=searchpass)
+        search_pass_window = canvas.create_window(
+            780, 240, anchor="nw", window=search_pass)
+
+        gen_pass = ttk.Button(
+            window, text="Generate Password", command=gen_pass)
+        gen_pass_window = canvas.create_window(
+            700, 300, anchor="nw", window=gen_pass)
+
+        add_button = ttk. Button(
+            window, text="Add", width=35, command=save_pass)
+        add_button_window = canvas.create_window(
+            550, 330, anchor="nw", window=add_button)
+
+        clear_button = ttk.Button(window, text="Reset",
+                                  width=35, command=reset_pass)
+        clear_button_window = canvas.create_window(
+            550, 360, anchor="nw", window=clear_button)
+
+        # view_pass = ttk.Button(
+        #     window, text="View Password", width=35, command=view_txt)
+        # view_pass_window = canvas.create_window(
+        #     550, 360, anchor="nw", window=view_pass)
+
+        # del_file = ttk.Button(window, text="Delete File",
+        #                       width=35, command=del_f)
+        # del_file_window = canvas.create_window(
+        #     550, 420, anchor="nw", window=del_file)
+
+        window.protocol("WM_DELETE_WINDOW", on_closing)
+        window.mainloop()
+        break
+
+    else:
+        incorrect_tries += 1
+        master_pass = simpledialog.askstring(
+            title='Prompt', prompt='Password Incorrect.Enter the master password again?', show="*")
+
+if (incorrect_tries > 2):
+    messagebox.showinfo(
+        title="Warning", message="Incorrect Password entered 3 times")
+    window.destroy()
